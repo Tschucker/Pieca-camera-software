370ecfb8e0885d64429078f830a23086260d82a5
tschucker <tdsepsilon@gmail.com>
2022-06-07 21:07:26 -0700

    Fixed install paths for new name.

--- install-camera.sh
+++ install-camera.sh
@@ -25,19 +25,19 @@ sudo chmod +x camera.py
 echo ''
 echo -e '\033[93mDownloading color profiles... \033[0m'
 cd ~
-sudo rm -Rf ~/camera/profiles
-mkdir ~/camera/profiles
-sudo chown -R $USER:$USER ~/camera/profiles
-wget -q https://github.com/davidplowman/Colour_Profiles/raw/master/imx477/PyDNG_profile.dcp -O ~/camera/profiles/basic.dcp
-wget -q https://github.com/davidplowman/Colour_Profiles/raw/master/imx477/Raspberry%20Pi%20High%20Quality%20Camera%20Lumariver%202860k-5960k%20Neutral%20Look.dcp -O ~/camera/profiles/neutral.dcp
-wget -q https://github.com/davidplowman/Colour_Profiles/raw/master/imx477/Raspberry%20Pi%20High%20Quality%20Camera%20Lumariver%202860k-5960k%20Skin%2BSky%20Look.dcp -O ~/camera/profiles/skin-and-sky.dcp
+sudo rm -Rf ~/Pieca-camera-software/profiles
+mkdir ~/Pieca-camera-software/profiles
+sudo chown -R $USER:$USER ~/Pieca-camera-software/profiles
+wget -q https://github.com/davidplowman/Colour_Profiles/raw/master/imx477/PyDNG_profile.dcp -O ~/Pieca-camera-software/profiles/basic.dcp
+wget -q https://github.com/davidplowman/Colour_Profiles/raw/master/imx477/Raspberry%20Pi%20High%20Quality%20Camera%20Lumariver%202860k-5960k%20Neutral%20Look.dcp -O ~/Pieca-camera-software/profiles/neutral.dcp
+wget -q https://github.com/davidplowman/Colour_Profiles/raw/master/imx477/Raspberry%20Pi%20High%20Quality%20Camera%20Lumariver%202860k-5960k%20Skin%2BSky%20Look.dcp -O ~/Pieca-camera-software/profiles/skin-and-sky.dcp
 
 cd ~
 echo ''
 echo -e '\033[93mSetting up alias... \033[0m'
 sudo touch ~/.bash_aliases
 sudo sed -i '/\b\(function camera\)\b/d' ~/.bash_aliases
-sudo sed -i '$ a function camera { sudo python3 ~/camera/camera.py "$@"; }' ~/.bash_aliases
+sudo sed -i '$ a function camera { sudo python3 ~/Pieca-camera-software/camera.py "$@"; }' ~/.bash_aliases
 echo -e 'You may use \e[1mcamera <options>\e[0m to launch the program.'
 
 echo ''
